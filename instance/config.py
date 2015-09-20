1e005dc7d41b54459761dde9169581993cb15ebe
ken8203 <ken8203@gmail.com>
2015-09-21 00:42:15 +0800

    add prototye

---
+++ instance/config.py
@@ -0,0 +1,2 @@
+SECRET_KEY = 'jay!@2z,lepd#$%^&*()>><<"'
+SQLALCHEMY_DATABASE_URI = "mysql://user:password@localhost/dbname"
---
+++ run.py
@@ -0,0 +1,4 @@
+from myapp import app
+
+#db.create_all()
+app.run(host='0.0.0.0', port=5000)
